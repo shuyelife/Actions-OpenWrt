c6ba36461aaa285407533d4ad135f70a5390ad24
树叶--摄影爱好者 <shuyelife@gmail.com>
2026-04-26 15:58:14 +0800

    Modify Makefile for 5G modem compatibility
    Updated Makefile dependencies and architecture settings for 5G modem packages.

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,19 +8,11 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 强行修正依赖：不管它在 Makefile 里写的是 +quectel-cm 还是 +quectel-cm-5G
+# 统一改为 quectel_cm_5G（对应你仓库里真实的拨号器包名）
+find feeds/5gmodem -name "Makefile" | xargs sed -i 's/+quectel-cm.*/+quectel_cm_5G/g'
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-# 1. 忽略大小写精准替换依赖包名，去掉 .* 防止误删后续依赖
-find feeds/5gmodem -name "Makefile" | xargs sed -i 's/+quectel-cm/+quectel_cm_5G/gI'
-find package/feeds/5gmodem -name "Makefile" | xargs sed -i 's/+quectel-cm/+quectel_cm_5G/gI'
-
-# 2. 修正架构为 all（即使该配置没选 H1，留着这行也不会报错）
-find feeds/5gmodem/luci-app-hypermodem -name "Makefile" | xargs sed -i 's/PKG_ARCHITECTURE:=.*/PKG_ARCHITECTURE:=all/g'
+# 2. 强行修正架构：直接把 5gmodem 目录下所有插件的架构设为 all
+# 这能彻底解决 "incompatible architecture" 的报错，让它在 MT7981 上畅通无阻
+find feeds/5gmodem -name "Makefile" | xargs sed -i 's/PKG_ARCHITECTURE:=.*/PKG_ARCHITECTURE:=all/g'

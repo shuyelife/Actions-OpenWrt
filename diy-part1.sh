4acbfbbdac4ef4ca9b4b139bb9188f3c38a2272c
树叶--摄影爱好者 <shuyelife@gmail.com>
2026-04-29 22:16:50 +0800

    Update diy-part1.sh to modify 5G modem setup
    Comment out the git clone command for 5G-Modem-Support and add instructions for removing it. Add a new feed source for QModem.

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,10 @@
 
 # Add a feed source
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone --depth 1 https://github.com/Siriling/5G-Modem-Support package/5gmodem
+#git clone --depth 1 https://github.com/Siriling/5G-Modem-Support package/5gmodem
+# 1. 彻底删除你之前 diy-part1.sh 里的这一行（重要，防止双重冲突）
+# sed -i '/5G-Modem-Support/d' diy-part1.sh (如果你之前是 sed 进去的)
+# 或者直接在文件中删除 git clone Siriling 的那一行
+
+# 2. 按照 QModem 官方标准，添加 feed 源
+echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default

4897150a7261b1f9aab78525534424744f2438fc
树叶--摄影爱好者 <shuyelife@gmail.com>
2026-04-30 16:07:36 +0800

    Create diy-part11.sh for OpenWrt feed configuration

---
+++ diy-part11.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#git clone --depth 1 https://github.com/Siriling/5G-Modem-Support package/5gmodem
+# 1. 彻底删除你之前 diy-part1.sh 里的这一行（重要，防止双重冲突）
+#sed -i '/5G-Modem-Support/d' diy-part1.sh (如果你之前是 sed 进去的)
+# 或者直接在文件中删除 git clone Siriling 的那一行
+
+# 2. 按照 QModem 官方标准，添加 feed 源
+#echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
+#echo 'src-git modem https://github.com/Siriling/5G-Modem-Support.git' >> feeds.conf.default
+# 2. 恢复并添加 Siriling 的 5G-Modem-Support 仓库
+echo 'src-git modem https://github.com/Siriling/5G-Modem-Support.git' >> feeds.conf.default

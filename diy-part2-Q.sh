bc3172e79274dc032acd7aa95c55691a0757c2f8
树叶--摄影爱好者 <shuyelife@gmail.com>
2026-05-02 07:10:22 +0800

    Remove manual installation of luci-app-adguardhome
    Removed manual installation of luci-app-adguardhome from feeds.

--- diy-part2-Q.sh
+++ diy-part2-Q.sh
@@ -21,7 +21,6 @@ echo "CONFIG_PACKAGE_luci-theme-argon=n" >> .config
 
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 # 强制修补：如果安装脚本漏掉了面板，我们手动把它拎出来
-./scripts/feeds install luci-app-adguardhome
 # 5. 不要手动拉取 OpenClash，让 feeds 自动处理，除非你确定要用 master 分支
 # 如果一定要换，就在 ncm.txt 里写清楚，不要在脚本里 rm -rf 物理覆盖
 

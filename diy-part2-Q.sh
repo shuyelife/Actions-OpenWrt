d13c70e6c28ce6b64f7232b4513bc5a1001c0ae5
树叶--摄影爱好者 <shuyelife@gmail.com>
2026-05-02 07:22:45 +0800

    Update configuration and remove unwanted packages
    Remove unnecessary packages from configuration and add AdGuard Home.

--- diy-part2-Q.sh
+++ diy-part2-Q.sh
@@ -6,11 +6,6 @@ cp -f defconfig/mt7981-ax3000.config .config
 sed -i '/CONFIG_TARGET_mediatek_mt7981_DEVICE_/d' .config
 echo "CONFIG_TARGET_mediatek_mt7981_DEVICE_cmcc_rax3000m=y" >> .config
 
-# 2. 【核心修改】不要删除源码，用 sed 禁用掉那些你不想要的包
-# 这样做能保住依赖树完整，同时固件里又不会出现它们
-sed -i 's/CONFIG_PACKAGE_luci-app-upnp=y/CONFIG_PACKAGE_luci-app-upnp=n/' .config
-sed -i 's/CONFIG_PACKAGE_luci-app-samba4=y/CONFIG_PACKAGE_luci-app-samba4=n/' .config
-
 # 3. 既然你要用 Bootstrap，直接在配置里指定，不要删 Argon
 echo "CONFIG_PACKAGE_luci-theme-bootstrap=y" >> .config
 echo "CONFIG_PACKAGE_luci-theme-argon=n" >> .config
@@ -18,6 +13,11 @@ echo "CONFIG_PACKAGE_luci-theme-argon=n" >> .config
 # 4. 更新源并安装 (顺序不要乱)
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+# 精准物理切除不需要的包 (绝不碰 luci-theme-argon，保证 WiFi 菜单存活)
+rm -rf feeds/luci/applications/luci-app-upnp
+rm -rf feeds/luci/applications/luci-app-usb-printer
+rm -rf feeds/luci/applications/luci-app-samba4
+rm -rf feeds/luci/applications/luci-app-ksmbd
 
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 # 强制修补：如果安装脚本漏掉了面板，我们手动把它拎出来
